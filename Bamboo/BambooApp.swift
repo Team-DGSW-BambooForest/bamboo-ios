00a64e3a68654ac4dc34b5e54404aceb38b63102
Mercen <mercen@mercen.net>
2022-11-29 10:00:20 +0900

    Initial Commit

---
+++ Bamboo/BambooApp.swift
@@ -0,0 +1,17 @@
+//
+//  BambooApp.swift
+//  Bamboo
+//
+//  Created by Mercen on 2022/11/29.
+//
+
+import SwiftUI
+
+@main
+struct BambooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bamboo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Bamboo
+//
+//  Created by Mercen on 2022/11/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
