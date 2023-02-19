7da4dd68ae813aaa32c438ebb7de5c55af749bc6
Mercen <mercen@mercen.net>
2023-02-19 22:28:39 +0900

    📝 API 주소 추가

---
+++ Bamboo/Resources/Constants/Constant.swift
@@ -0,0 +1,10 @@
+//
+//  Constant.swift
+//  Bamboo
+//
+//  Created by Mercen on 2023/02/19.
+//
+
+public let baseAPI: String = "http://13.209.33.18:8080"
+public let userAPI: String = "\(baseAPI)/user"
+public let postAPI: String = "\(baseAPI)/post"
