0236810c4332f43aca107ab3cdf7db1445bda195
Mercen <mercen@mercen.net>
2023-02-26 14:11:33 +0900

    ✨ 댓글과 답글 불러오기

--- Bamboo/Resources/Network/Constants.swift
+++ Bamboo/Resources/Network/Constants.swift
@@ -9,4 +9,5 @@
 public let baseAPI: String = "http://13.209.33.18:8080"
 public let userAPI: String = "\(baseAPI)/user"
 public let postAPI: String = "\(baseAPI)/post"
+public let commentAPI: String = "\(baseAPI)/comment"
 public let uploadAPI: String = "\(baseAPI)/upload"
--- Bamboo/Sources/Post/PostModel.swift
+++ Bamboo/Sources/Post/PostModel.swift
@@ -7,11 +7,33 @@
 
 import SwiftUI
 
+struct Comment: Codable, Hashable {
+    let commentId: Int
+    let profileImage: String
+    let writer: String
+    let content: String
+    let diffTime: String
+}
+
+struct FullComment: Codable, Hashable {
+    let comment: Comment
+    let nested: [Comment]
+}
+
 class PostModel: ObservableObject {
     @Published var post: Post?
+    @Published var comments = [FullComment]()
     func loadData(_ id: String) {
         request("\(postAPI)/\(id)", .get, Post.self) { data in
             self.post = data
+        }
+        request("\(commentAPI)/\(id)", .get, [Comment].self) { datas in
+            datas.forEach { comment in
+                request("\(commentAPI)/nested/\(comment.commentId)", .get, [Comment].self) { nested in
+                    self.comments.append(FullComment(comment: comment, nested: nested))
+                }
+            }
+            print(self.comments)
         }
     }
 }
